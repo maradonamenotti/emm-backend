838b4c4e34a396ac6550948381acb7bae896bb9f
mcastroa7 <manucastroa7@gmail.com>
2026-06-04 13:16:12 -0300

    Fix: Remove test root files that offset TS compilation output directory

--- scratch/test_token.js
+++
@@ -1,5 +0,0 @@
-const token = "EABAyu2qw7GYBRnWY8e68Wds5y2x6IriDwIrpJZBZBB1dj1tmdYZAvRaK22Dg8RZBJshoqSftwXpCSN6BZC31glNDd9gqY9X1cnfm6OJBFaPa3NXuWo2xIPB2meSCKfKvCcUFonZBrMMo2pUViLyfnCgfcVreQEWsq2LyHiZCwJVJJZCTFFI3EBdZCPh7suIF5UO9CGJU33rCm3TEmnHzaPswXQiwkhikOH2zOOOJc8FmWqyAsmL9AqpW1Dt7wCJUtaeKeAXIhCzToesnwhtahnFz90CONip2AKMjA";
-fetch(`https://graph.facebook.com/v19.0/debug_token?input_token=${token}&access_token=${token}`)
-  .then(r => r.json())
-  .then(console.log)
-  .catch(console.error);
--- script.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg'); 
-const client = new Client({ connectionString: 'postgresql://postgres:Riverplate912@localhost:5432/analiticos' }); 
-client.connect().then(async () => { 
-  const res = await client.query("UPDATE student SET en_analiticos = true WHERE id IN (SELECT student_id FROM nota) OR estado_analitico = 'emitido' OR situacion = 'CREADO MANUAL'"); 
-  console.log('Updated', res.rowCount, 'students'); 
-  client.end(); 
-}).catch(console.error);
