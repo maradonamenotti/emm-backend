628465af1d2415a42c61569c8adb9fcbfc362bda
mcastroa7 <manucastroa7@gmail.com>
2026-06-04 15:10:20 -0300

    Fix: Add Student repository to WhatsAppModule imports

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -4,13 +4,17 @@ import { Prospecto } from '../entities/Prospecto';
 import { MensajeWhatsApp } from '../entities/MensajeWhatsApp';
 import { EstadoEmbudo } from '../entities/EstadoEmbudo';
 import { CrmPlantilla } from '../entities/CrmPlantilla';
+import { Student } from '../entities/Student';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppGateway } from './whatsapp.gateway';
 import { WhatsAppService } from './whatsapp.service';
 import { AiTriageService } from './ai-triage.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Prospecto, MensajeWhatsApp, EstadoEmbudo, CrmPlantilla], 'crm')],
+    imports: [
+        TypeOrmModule.forFeature([Student]),
+        TypeOrmModule.forFeature([Prospecto, MensajeWhatsApp, EstadoEmbudo, CrmPlantilla], 'crm')
+    ],
     controllers: [WhatsAppController],
     providers: [WhatsAppService, WhatsAppGateway, AiTriageService],
     exports: [WhatsAppService],
